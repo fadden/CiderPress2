d49bcf1954c9bfd5deb92dc03436966382e4451e
Andy McFadden <fadden@fadden.com>
2023-11-24 10:42:45 -0800

    Replace WPF hack with other WPF hack
    This replaces the "dummy element" hack with the "binding proxy"
    hack.  This is required to be able to hide DataGrid columns.
    
    The goal was to improve the problems seen in issue #5, but this
    doesn't seem to change that behavior.  (Technically, the behavior
    has changed: the infinite GC issue isn't happening, and there's a
    new series of binding complaints.  But that was true before I
    started making changes.)
    
    Yay WPF.

---
+++ cp2_wpf/WPFCommon/BindingProxy.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Copyright 2023 faddenSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Windows;
+
+namespace cp2_wpf.WPFCommon {
+    /// <summary>
+    /// From <see href="https://stackoverflow.com/a/22074985/294248"/>.  Used to allow
+    /// access to dependency properties in DataGrid columns.
+    /// </summary>
+    public class BindingProxy : Freezable {
+        protected override Freezable CreateInstanceCore() {
+            return new BindingProxy();
+        }
+
+        public object Data {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy));
+    }
+}
