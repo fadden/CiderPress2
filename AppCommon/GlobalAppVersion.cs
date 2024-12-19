61c1869a67ea21e49a12c10ef59418f53c1683c7
Andy McFadden <fadden@fadden.com>
2024-12-19 09:45:43 -0800

    Version 1.0.6-dev1

--- AppCommon/GlobalAppVersion.cs
+++ AppCommon/GlobalAppVersion.cs
@@ -27,7 +27,7 @@ namespace AppCommon {
         /// Overall application version.
         /// </summary>
         public static readonly CommonUtil.Version AppVersion =
-            new CommonUtil.Version(1, 0, 5, CommonUtil.Version.PreRelType.Final, 0);
+            new CommonUtil.Version(1, 0, 6, CommonUtil.Version.PreRelType.Dev, 1);
         // NOTE: there's an independent version in DiskArc/Defs.cs
     }
 }
